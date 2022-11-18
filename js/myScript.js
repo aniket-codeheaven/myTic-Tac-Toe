168dfdb589a224e7cae648b2b4caa50da029c1a8
Aniket <57931002+aniket-codeheaven@users.noreply.github.com>
2022-11-18 11:51:03 +0530

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -106,13 +106,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("1 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[3]==myJSON[4]) && (myJSON[4]==myJSON[5])){
             if(myJSON[3]==1){
@@ -120,13 +122,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("2 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[6]==myJSON[7]) && (myJSON[7]==myJSON[8])){
             if(myJSON[6]==1){
@@ -134,13 +138,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("3 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[0]==myJSON[4]) && (myJSON[4]==myJSON[8])){
             if(myJSON[0]==1){
@@ -148,13 +154,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("4 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[6]==myJSON[4]) && (myJSON[4]==myJSON[2])){
             if(myJSON[6]==1){
@@ -162,13 +170,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("5 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         }  else if((myJSON[0]==myJSON[3]) && (myJSON[3]==myJSON[6])){
             if(myJSON[0]==1){
@@ -176,13 +186,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("6 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[1]==myJSON[4]) && (myJSON[4]==myJSON[7])){
             if(myJSON[1]==1){
@@ -190,13 +202,15 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("7 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[2]==myJSON[5]) && (myJSON[5]==myJSON[8])){
             if(myJSON[2]==1){
@@ -204,26 +218,30 @@ $(document).ready(function () {
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(255, 0, 0)"});
                 $('#body-color').css("background-color" , "rgb(255, 0, 0)");
-                $('.postmatch').children('h1').html("RED PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("RED PLAYER WINS");
             } else{
                 console.log("8 else green wins");
                 $('.ttt-grid').css("display","none");
                 $('.postmatch').css({"display":"block", "display":"flex", "background-color":"rgb(0, 255, 0)"});
                 $('#body-color').css("background-color" , "rgb(0, 255, 0)");
-                $('.postmatch').children('h1').html("GREEN PLAYER WINS")
+                $('#body-color').children('h5').html(" ");
+                $('.postmatch').children('h1').html("GREEN PLAYER WINS");
             }
         } else if((myJSON[0]+myJSON[1]+myJSON[1]+myJSON[2]+myJSON[3]+myJSON[4]+myJSON[5]+myJSON[6]+myJSON[7]+myJSON[8]) > 4){
             console.log(i);
             $('.ttt-grid').css("display","none");
             $('.postmatch').css({"display":"block", "display":"flex"});
-            $('.postmatch').children('h1').html("MATCH IS TIED")
+            $('.postmatch').children('h1').html("MATCH IS TIED");
             $('#body-color').css("background-color" , "rgb(219, 112, 147)");
             // $('#body-color').css("background-color" , "rgb(255, 255, 0)");
+            $('#body-color').children('h5').html(" ");
 
         }
     }
 
     $('#body-color').css("background-color" , "rgb(255, 0, 0)");
+    $('#body-color').children('h5').html("red's turn");
 
     $('.box-tiles').click(function (){
         if($(this).hasClass('cross') || $(this).hasClass('ooo')){
@@ -234,6 +252,7 @@ $(document).ready(function () {
             if($(this).css("background-color") === "rgb(255, 255, 255)"){
                 if($('#body-color').css("background-color") === "rgb(255, 0, 0)"){
                     $('#body-color').css("background-color" , "rgb(0, 255, 0)");
+                    $('#body-color').children('h5').html("green's turn");
                     // $(this).css("background-color" , "rgb(255, 0, 0)");
                     $(this).addClass('cross');
                     // $(this).children().addClass('cross');
@@ -245,6 +264,7 @@ $(document).ready(function () {
                 } 
                 else if($('#body-color').css("background-color") === "rgb(0, 255, 0)"){
                     $('#body-color').css("background-color" , "rgb(255, 0, 0)");
+                    $('#body-color').children('h5').html("red's turn");
                     // $(this).css("background-color" , "rgb(0, 255, 0)");
                     // $(this).addClass('ooo');
                     if(!$(this).hasClass('cross')){
